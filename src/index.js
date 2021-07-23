103f1d62dfc13899849a3bb3c4c3cd96efcf7c14
Enrique Alejandro <kikeaag@MacBook-Air-de-Enrique.local>
2021-07-23 17:12:26 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootswatch/dist/minty/bootstrap.min.css';
+
+import 'bootstrap';
+//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
